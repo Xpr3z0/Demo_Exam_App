dd32397230424b1ec745fd66ff612002a6f14cce
Xpr3z0 <lol100904yt@gmail.com>
2024-02-18 15:01:48 +0200

    QR Code generation + Added QR Code sample on Settings Tab + Added locally QR Code generation library

--- src/main/java/com/example/controller/tabs/SettingsTabController.java
+++ src/main/java/com/example/controller/tabs/SettingsTabController.java
@@ -1,16 +1,44 @@
 package com.example.controller.tabs;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class SettingsTabController implements Initializable {
-    public AnchorPane settingsTab;
+    private final String QR_CODE_URL = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    public BorderPane settingsTab;
+    public ImageView qrImageView;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        qrImageView.setImage(generateQRCode(QR_CODE_URL));
+    }
 
+    private Image generateQRCode(String content) {
+        try {
+            // Используем MultiFormatWriter из ZXing для генерации QR-кода
+            BitMatrix bitMatrix = new MultiFormatWriter()
+                    .encode(content, BarcodeFormat.QR_CODE, 300, 300);
+
+            // Преобразование BitMatrix в Image
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            MatrixToImageWriter.writeToStream(bitMatrix, "PNG", out);
+
+            return new Image(new ByteArrayInputStream(out.toByteArray()));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
