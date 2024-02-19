bd771435f5aed439bbd6b886feddfa60d8d63ea2
Xpr3z0 <lol100904yt@gmail.com>
2024-02-19 11:18:45 +0200

    added id in NewRequestTab.fxml

--- src/main/java/com/example/controller/tabs/NewRequestTabController.java
+++ src/main/java/com/example/controller/tabs/NewRequestTabController.java
@@ -2,12 +2,13 @@ package com.example.controller.tabs;
 
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class NewRequestTabController implements Initializable {
-    public AnchorPane newRequestTab;
+    public BorderPane root;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
