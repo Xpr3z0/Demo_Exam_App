20963dbb068d69b57f4888ac02a9d4425af0512a
Xpr3z0 <lol100904yt@gmail.com>
2024-04-16 17:10:10 +0200

    implemented logic for filters for manager

--- src/main/java/com/example/controller/manager_tabs/AllRequestsTabController.java
+++ src/main/java/com/example/controller/manager_tabs/AllRequestsTabController.java
@@ -3,13 +3,16 @@ package com.example.controller.manager_tabs;
 import com.example.bdclient.ClientPostgreSQL;
 import com.example.controller.ListItemController;
 import com.example.controller.dialogs.MyAlert;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.util.StringConverter;
 
 import java.io.IOException;
@@ -17,9 +20,15 @@ import java.net.URL;
 import java.sql.*;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class AllRequestsTabController implements Initializable {
+    public VBox statesVBox;
+    public VBox priorityVBox;
+    public TextField dateFilterTF;
+    public TextField idFilterTF;
 
     @FXML
     private ListView<String> repairRequestListView;
@@ -45,13 +54,25 @@ public class AllRequestsTabController implements Initializable {
     private final String LOGIN = "postgres";
     private final String PASSWORD = "root";
     private Connection connection = null;
+    private String initialQuery;
+    private String query;
     private int currentRequestNumber = -1;
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         moreInfoPane.setVisible(false);
-        loadRepairRequests();
+
+        try {
+            connection = DriverManager.getConnection(DB_URL, LOGIN, PASSWORD);
+            initialQuery = "SELECT request_number FROM repair_requests WHERE state != 'Новая' ORDER BY request_number";
+            query = initialQuery;
+            loadRepairRequests(connection, query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            MyAlert.showErrorAlert("Ошибка при соединении с базой данных.");
+        }
+
         priorityChoice.getItems().addAll("Срочный", "Высокий", "Нормальный", "Низкий");
         stateChoice.getItems().addAll("В работе", "Выполнено", "В ожидании");
 
@@ -68,57 +89,169 @@ public class AllRequestsTabController implements Initializable {
         });
     }
 
+    @FXML
+    public void applyFilters(ActionEvent event) {
+        repairRequestListView.getItems().clear(); // Очищаем ListView перед загрузкой новых данных
 
-    private void loadRepairRequests() {
-        // Здесь вы должны использовать JDBC для подключения к вашей БД
-        // и получения данных о заявках на ремонт
+        StringBuilder queryBuilder = new StringBuilder("SELECT request_number FROM repair_requests WHERE state != 'Новая' AND ");
 
-        try (Connection connection = DriverManager.getConnection(DB_URL, LOGIN, PASSWORD)) {
-            String query = "SELECT request_number FROM repair_requests WHERE state != 'Новая' ORDER BY request_number";
+        List<String> conditions = new ArrayList<>();
 
-
-            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-                try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                    while (resultSet.next()) {
-                        // Добавляем элементы в ListView
-                        String requestNumber = resultSet.getString("request_number");
-
-                        repairRequestListView.getItems().add(requestNumber);
-                        repairRequestListView.setCellFactory(param -> {
-                            return new ListCell<String>() {
-                                @Override
-                                protected void updateItem(String item, boolean empty) {
-                                    super.updateItem(item, empty);
-
-                                    if (item == null || empty) {
-                                        setGraphic(null);
-                                    } else {
-                                        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/ListItem.fxml"));
-                                        try {
-                                            Parent root = loader.load();
-                                            ListItemController controller = loader.getController();
-                                            controller.setRequestNumber(Integer.parseInt(item));
-                                            setGraphic(root);
-                                        } catch (IOException e) {
-                                            e.printStackTrace();
-                                            setGraphic(null);
-                                        }
-                                    }
-                                }
-                            };
-                        });
-                    }
+        // Фильтры по состояниям (используем оператор OR между выбранными состояниями)
+        List<String> stateConditions = new ArrayList<>();
+        for (Node node : statesVBox.getChildren()) {
+            if (node instanceof CheckBox) {
+                CheckBox checkbox = (CheckBox) node;
+                if (checkbox.isSelected()) {
+                    stateConditions.add("state = '" + checkbox.getText() + "'");
                 }
+            }
+        }
+        if (!stateConditions.isEmpty()) {
+            conditions.add("(" + String.join(" OR ", stateConditions) + ")");
+        }
+
+        // Фильтры по приоритетам (используем оператор OR между выбранными приоритетами)
+        List<String> priorityConditions = new ArrayList<>();
+        for (Node node : priorityVBox.getChildren()) {
+            if (node instanceof CheckBox) {
+                CheckBox checkbox = (CheckBox) node;
+                if (checkbox.isSelected()) {
+                    priorityConditions.add("priority = '" + checkbox.getText() + "'");
+                }
+            }
+        }
+        if (!priorityConditions.isEmpty()) {
+            conditions.add("(" + String.join(" OR ", priorityConditions) + ")");
+        }
+
+        // Фильтр по дате создания заявки
+        String selectedDate = dateFilterTF.getText();
+        if (selectedDate != null && !selectedDate.trim().isEmpty()) {
+            conditions.add("register_date = '" + selectedDate + "'");
+        }
+
+        // Фильтр по номеру заявки
+        String requestNumber = idFilterTF.getText().trim();
+        if (!requestNumber.isEmpty()) {
+            try {
+                int requestNum = Integer.parseInt(requestNumber);
+                conditions.add("request_number = " + requestNum);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                MyAlert.showErrorAlert("Неверный формат номера заявки.");
+                return;
+            }
+        }
+
+        // Строим окончательное условие WHERE
+        if (!conditions.isEmpty()) {
+            queryBuilder.append(String.join(" AND ", conditions));
+        } else {
+            queryBuilder.append("1=1"); // Если фильтры не выбраны, выбираем все заявки
+        }
+
+        queryBuilder.append(" ORDER BY request_number");
+
+        // Выполняем SQL-запрос и заполняем ListView
+        try (Connection connection = DriverManager.getConnection(DB_URL, LOGIN, PASSWORD)) {
+            query = queryBuilder.toString();
+            loadRepairRequests(connection, query);
+
+            // Проверяем, видима ли подробная информация после применения фильтров
+            if (!isRequestNumberInFilteredResults(currentRequestNumber)) {
+                moreInfoPane.setVisible(false);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            MyAlert.showErrorAlert("Ошибка при соединении с базой данных.");
+        }
+
+        MyAlert.showInfoAlert("Фильтры успешно применены");
+    }
+
+
+    // Метод для проверки, присутствует ли номер заявки в отфильтрованном списке
+    private boolean isRequestNumberInFilteredResults(int requestNumber) {
+        ObservableList<String> filteredItems = repairRequestListView.getItems();
+        for (String item : filteredItems) {
+            if (Integer.parseInt(item) == requestNumber) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @FXML
+    public void clearFilters(ActionEvent event) {
+        // Очистка выбора с чекбоксов состояний
+        for (Node node : statesVBox.getChildren()) {
+            if (node instanceof CheckBox) {
+                ((CheckBox) node).setSelected(false);
+            }
+        }
+
+        // Очистка выбора с чекбоксов приоритета
+        for (Node node : priorityVBox.getChildren()) {
+            if (node instanceof CheckBox) {
+                ((CheckBox) node).setSelected(false);
+            }
+        }
+
+        // Очистка полей под дату и номер заявки
+        dateFilterTF.clear();
+        idFilterTF.clear();
+    }
+
+
+
+    private void loadRepairRequests(Connection connection, String query) {
+        this.query = query;
+        repairRequestListView.getItems().clear(); // Очищаем ListView перед загрузкой новых данных
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            while (resultSet.next()) {
+                // Добавляем элементы в ListView
+                String requestNumber = resultSet.getString("request_number");
+
+                repairRequestListView.getItems().add(requestNumber);
+                repairRequestListView.setCellFactory(param -> {
+                    return new ListCell<String>() {
+                        @Override
+                        protected void updateItem(String item, boolean empty) {
+                            super.updateItem(item, empty);
+
+                            if (item == null || empty) {
+                                setGraphic(null);
+                            } else {
+                                FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/ListItem.fxml"));
+                                try {
+                                    Parent root = loader.load();
+                                    ListItemController controller = loader.getController();
+                                    controller.setRequestNumber(Integer.parseInt(item));
+                                    setGraphic(root);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                    setGraphic(null);
+                                }
+                            }
+                        }
+                    };
+                });
             }
         } catch (SQLException e) {
             e.printStackTrace();
+            MyAlert.showErrorAlert("Ошибка при загрузке данных.");
         }
     }
 
     @FXML
     public void onActionRefresh(ActionEvent event) {
         repairRequestListView.getItems().clear();
-        loadRepairRequests();
+        loadRepairRequests(connection, query);
     }
 
 
